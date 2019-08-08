347a6d95127cf11d521ab007b4563d2e5b52cd1c
yirrk <1295795230@qq.com>
2019-08-08 11:03:54 +0800

    something improved in views and test

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+import datetiome
+from django.utils import timezone
+from .models import Question
 
-# Create your tests here.
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        '''
+        当问卷发布是未来某天时，was_published_recently()会返回False
+        '''
+        time = timezone.now() + datetiome.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
--- polls/views.py
+++ polls/views.py
@@ -1,36 +1,32 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 # from django.template import loader
 from .models import Question, Choice
-from django.http import Http404
+# from django.http import Http404
 from django.urls import reverse
+from django.views import generic
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-# template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-        }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    """docstring for IndexView"""
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        ''' 返回最新的5个问卷 '''
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    '''try:
-            question = Question.objects.get(pk=question_id)
-        except Question.DoesNotExist:
-            raise Http404("问卷不存在")'''
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+        """docstring for DetailView"""
+        model = Question
+        template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
-
-def vote(request, question_id):
-    return HttpResponse("请为问卷 %s 提交您的答案" % question_id)
+class ResultsView(generic.DetailView):
+    """docstring for ResultsView"""
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
